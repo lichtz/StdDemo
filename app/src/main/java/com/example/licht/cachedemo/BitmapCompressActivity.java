8f6710ab478a9036edbe1118d37f9b7782db93af
licht <362829410@qq.com>
2018-12-05 01:13:27 +0800

    commit

--- app/src/main/java/com/example/licht/cachedemo/BitmapCompressActivity.java
+++ app/src/main/java/com/example/licht/cachedemo/BitmapCompressActivity.java
@@ -2,8 +2,10 @@ package com.example.licht.cachedemo;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Canvas;
+import android.graphics.RectF;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import java.io.File;
@@ -27,4 +29,14 @@ public class BitmapCompressActivity extends AppCompatActivity {
 //        bmp.compress(Bitmap.CompressFormat.WEBP,100,out)
 
     }
+
+    public static void comprssBitmapSize(Bitmap bmp,File file) {
+        Bitmap result = Bitmap.createBitmap(bmp.getWidth() / 2, bmp.getHeight() / 2, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(result);
+        RectF rectF = new RectF(0, 0, bmp.getWidth() / 2, bmp.getHeight() / 2);
+        canvas.drawBitmap(bmp,null,rectF,null);
+//        result.compress()
+
+    }
+
 }
